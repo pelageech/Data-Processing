e79e554ecd37f73e5afbe0a490c7a6ee58f7c001
pelageech <va.oleg2015@yandex.ru>
2023-09-26 09:31:14 +0700

    init

---
+++ Task_6/src/main/java/Founder.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Founder {
+    private final List<Runnable> workers;
+    public Founder(final Company company) {
+        this.workers = new ArrayList<>(company.getDepartmentsCount());
+    }
+    public void start() {
+        for (final Runnable worker : workers) {
+            new Thread(worker).start();
+        }
+    }
+}
