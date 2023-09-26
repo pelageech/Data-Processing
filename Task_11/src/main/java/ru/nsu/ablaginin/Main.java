e0c9ec58ceb24994488fb449e74f43506c6c9aa5
pelageech <va.oleg2015@yandex.ru>
2023-09-26 10:04:08 +0700

    task 11

---
+++ Task_11/src/main/java/ru/nsu/ablaginin/Main.java
@@ -0,0 +1,22 @@
+package ru.nsu.ablaginin;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static void main(String[] args) {
+        Semaphore s1 = new Semaphore(1);
+        Semaphore s2 = new Semaphore(0);
+        var th = new Printer(s1, s2);
+        th.start();
+
+        for (int i = 10; i < 20; i++) {
+            try {
+                s1.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+            s2.release();
+        }
+    }
+}
---
+++ Task_11/src/main/java/ru/nsu/ablaginin/Printer.java
@@ -0,0 +1,25 @@
+package ru.nsu.ablaginin;
+
+import java.util.concurrent.Semaphore;
+
+public class Printer extends Thread {
+    private final Semaphore s1;
+    private final Semaphore s2;
+
+    public Printer(Semaphore s1, Semaphore s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                s2.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+            s1.release();
+        }
+    }
+}
