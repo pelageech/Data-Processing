6297a526830859de9f64dd912511fc236021d2bb
pelageech <va.oleg2015@yandex.ru>
2023-10-03 15:15:51 +0700

    Неработающий код

--- Task_3/src/main/java/ru/nsu/ablaginin/Main.java
+++ Task_3/src/main/java/ru/nsu/ablaginin/Main.java
@@ -2,25 +2,16 @@ package ru.nsu.ablaginin;
 
 public class Main {
     static String[] s = new String[]{"A", "B", "C", "D"};
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         var Printer = new Printer();
         var th1 = new Thread(() -> Printer.print(s));
-        synchronized (s) {
             th1.start();
-            s.wait();
-        }
         s[0] = "Q";
         var th2 = new Thread(() -> Printer.print(s));
-        synchronized (s) {
             th2.start();
-            s.wait();
-        }
         s[1] = "W";
         var th3 = new Thread(() -> Printer.print(s));
-        synchronized (s) {
             th3.start();
-            s.wait();
-        }
         s[2] = "E";
         var th4 = new Thread(() -> Printer.print(s));
         th4.start();
--- Task_3/src/main/java/ru/nsu/ablaginin/Printer.java
+++ Task_3/src/main/java/ru/nsu/ablaginin/Printer.java
@@ -3,10 +3,12 @@ package ru.nsu.ablaginin;
 import java.util.Arrays;
 
 public class Printer {
-    public void print(String[] message) {
-        synchronized (message) {
-            System.out.println(Arrays.stream(message).toList());
-            message.notify();
+    public synchronized void print(String[] message) {
+        String[] m;
+        m = message.clone();
+        for (int i = 0; i < m.length; i++) {
+            m[i] = String.valueOf(m[i]);
         }
+        System.out.println(Arrays.stream(message).toList());
     }
 }
