1cca53cd18c401227d4d5ca45b606333a23fe36a
pelageech <va.oleg2015@yandex.ru>
2023-12-05 10:20:53 +0700

    unit test

--- Clojure_2/main.clj
+++ Clojure_2/main.clj
@@ -1,4 +1,5 @@
-(ns ru.nsu.ablaginin)
+(ns ru.nsu.ablaginin
+  (:require [clojure.test :refer :all]))
 
 (defn sieve [start]
   (cons (first start)
@@ -13,4 +14,19 @@
 
 (println (take 10 (pseq)))
 
+(deftest first-test
+  (testing "First Prime"
+    (is (== (nth (pseq) 0) 2))))
 
+(deftest hundred-test
+  (testing "100th Prime"
+    (is (== (nth (pseq) 99) 541))))
+
+(deftest thousand-test
+  (testing "1000st Prime"
+    (is (== (nth (pseq) 999) 7919))))
+
+
+(test #'first-test)
+(test #'hundred-test)
+(test #'thousand-test)
