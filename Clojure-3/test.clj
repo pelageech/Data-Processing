28bf7b7e0f6a046cfc3fd0c2caab1b87c0ea39a3
pelageech <va.oleg2015@yandex.ru>
2023-12-12 15:00:20 +0700

    modified:   Clojure-3/test.clj

--- Clojure-3/test.clj
+++ Clojure-3/test.clj
@@ -10,18 +10,13 @@
 (time (count (filter loong-func (range 1 1000))))
 (time (count (main/p-filter-lazy loong-func (range 1 1000))))
 
-(def A (filter even? (range 1 1000)))
-(def B (main/p-filter-lazy even? (range 1 1000)))
-(def C (take 10 (filter even? (range))))
-(def D (take 10 (main/p-filter-lazy even? (range))))
-
 (deftest finite-test
   (testing "Finite Sequence Filtering"
-    (is (= A B))))
+    (is (= (filter even? (range 1 1000)) (main/p-filter-lazy even? (range 1 1000))))))
 
 (deftest lazy-test
   (testing "Laziness"
-    (is (= C D))))
+    (is (= (take 10 (filter even? (range))) (take 10 (main/p-filter-lazy even? (range)))))))
 
 (test #'finite-test)
 (println "--- finite-test: PASS")
